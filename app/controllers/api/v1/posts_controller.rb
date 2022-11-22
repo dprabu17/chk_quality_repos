67be4b0aa8edfdaa852bfa293c88551474512b63
prabu <prabud@spritle.com>
2022-11-22 23:54:39 +0530

    add posts

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::PostsController < ApplicationController
+  def index
+    posts = Post.all
+    render json: posts, status: 200
+  end
+
+  def show
+    post = Post.find_by(id: params[:id])
+    if post
+      render json: post, status: 200
+    else
+      render json: {error: "Not found."}
+    end
+  end
+
+  def create
+    post = Post.new(title: post_params[:title], description: post_params[:description])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit([:title, :description])
+  end
+end
